cc617ceef553a4ec37dc195248fd2083b0aa8bfc
dogukanngumus <dogukanngumus@gmail.com>
2024-12-21 18:54:09 +0300

    refactorings added

--- main.py
+++ main.py
@@ -1,30 +1,53 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
 
-path_to_file = "books/frankenstein.txt"
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
 
-def count_characters(file_content):
-    lowered_file_content = list(file_content.lower())
-    char_dictionary = {}
-    for i in lowered_file_content:
-        if i in char_dictionary:
-            char_dictionary[i] += 1
-        else:
-            char_dictionary[i] = 1
-    return char_dictionary
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
 
-def print_report(word_count, char_count):
-    print(f"--- Begin report of {path_to_file} ---")
-    print(f"{word_count} words found in the document")
-    for i in char_count:
-        print(f"The '{i}' character was found {char_count[i]} times")
     print("--- End report ---")
 
-def main():
-    with open(path_to_file) as f:
-        file_contents = f.read()
-        word_count = len(file_contents.split())
-        char_count = count_characters(file_contents)
-        print_report(word_count, char_count)
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 
 main()
-    
